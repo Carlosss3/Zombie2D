42eebb8101a747ff9ec8008d7605b59e2dc8ed3d
Carlosss3 <carlosrojasfeixas@gmail.com>
2014-12-03 18:39:33 +0100

    Teletransporte y Cambio de nivel
    Puertas teletransporte
    Puertas cambio de nivel
    
    Script de ambas

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+	// Use this for initialization
+
+		public string nombreNivel;
+	public float tiempoEspera = 3.0f;
+		bool cargando = false;
+
+		void OnTriggerStay2D(Collider2D AvatarTarget){
+			if(AvatarTarget.transform.tag == "Player"){
+				//Application.LoadLevel(nombreNivel);
+			if(!cargando)
+				StartCoroutine(cargaNivel());
+		}
+
+	
+	}
+	
+	void OnTriggerExit2D(Collider2D target){
+				cargando = false;
+		}
+
+	IEnumerator cargaNivel(){			//Para parar la funcion a la que voy a llamar.
+				cargando = true;
+				yield return new WaitForSeconds (tiempoEspera);
+				if (cargando)
+						Application.LoadLevel (nombreNivel);
+		}
+}
