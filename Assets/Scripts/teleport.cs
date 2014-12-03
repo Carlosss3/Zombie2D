cedb45b10f0698f5354302f0395de688c02d9ddd
Carlosss3 <carlosrojasfeixas@gmail.com>
2014-12-03 17:23:36 +0100

    Teletransporte

--- Assets/Scripts/teleport.cs
+++ Assets/Scripts/teleport.cs
@@ -3,41 +3,57 @@ using System.Collections;
 
 public class teleport : MonoBehaviour {
 	
-	public Transform punto1;
-	public float tiempoEspera = 0.0f; // Tiempo que el Player tiene que estar para cargar
-	bool cargando = false; // Controla si el player esta dentro del trigger
+	public Transform destino;
+	public bool entrapuerta = false;
+	public bool teletransporta = false;
+	//public float tiempoEspera = 0.0f; // Tiempo que el Player tiene que estar para cargar
+	//bool cargando = false; // Controla si el player esta dentro del trigger
 	
-	
+	void Update(){
+				if (Input.GetKeyDown (KeyCode.E) && entrapuerta) {
+			teletransporta = true;
+				}
+		}
 	
 	void OnDrawGizmosSelected() {
-		if (punto1 != null) {
+		if (destino != null) {
 			Gizmos.color = Color.blue;
-			Gizmos.DrawLine(transform.position,  punto1.position);
+			Gizmos.DrawLine(transform.position,  destino.position);
 		}
 	}
 	
-	
+	void OnTriggerEnter2D(Collider2D target){
+				if (target.transform.tag == "Player")
+						entrapuerta = true;
+		}
 	void OnTriggerStay2D(Collider2D target){
-		
-		if (target.transform.tag == "Player") {
-			Debug.Log("Entrando");
-			if(!cargando) // Si no esta cargando empezamos la cuenta atras
-				StartCoroutine(cargaSalto(target));
-			
+				if (teletransporta) {
+						target.transform.position = destino.position;
+						teletransporta = false;
+			entrapuerta = false	;
+				}
 		}
+
+	void OnTriggerExit2D (Collider2D target){
+				if (target.transform.tag == "Player")
+						entrapuerta = false;
+		}
+		
+
 	}
-	
+	/*
 	//Si salimos del trigger cortamos la carga
 	void OnTriggerExit2D(Collider2D target){
 		cargando = false;
 	}
-	
+	*/
+	/*
 	// Ejecutamos esto cuando entramos en el trigger
 	IEnumerator cargaSalto(Collider2D target) {
 		cargando = true;
 		Debug.Log("Before Waiting 2 seconds");
 		yield return new WaitForSeconds(tiempoEspera); // Esperamos el tiempo definido
 		if(cargando) // Si sigue en el trigger cargamos la escena
-			target.transform.position = punto1.position;
+			target.transform.position = destino.position;
 	}
-}
+	*/
